081ede6448370a4eb6a449ef8117397629d1d735
Nick Kretz <nick@kretz.co>
2026-03-12 16:25:33 -0700

    Add initial structure for search console performance API

---
+++ api/search-console-performance.ts
@@ -0,0 +1,15 @@
+{
+  ok: true,
+  source: "google_search_console",
+  date_range: {},
+  pages: [
+    {
+      url
+      clicks
+      impressions
+      ctr
+      avg_position
+      top_queries: []
+    }
+  ]
+}
