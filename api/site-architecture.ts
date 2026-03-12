b9c99a14891e3706f9e5ebd817b3b95105e022a1
Nick Kretz <nick@kretz.co>
2026-03-12 16:25:51 -0700

    Add site architecture API response structure

---
+++ api/site-architecture.ts
@@ -0,0 +1,15 @@
+{
+  ok: true,
+  pages: [
+    {
+      url
+      internal_links_in
+      internal_links_out
+      crawl_depth
+      is_orphan
+      is_indexable
+      status_code
+      canonical_url
+    }
+  ]
+}
