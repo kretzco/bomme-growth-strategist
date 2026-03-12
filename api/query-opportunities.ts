7cadcdeaf74af198ed863441758eb7156c86e5c3
Nick Kretz <nick@kretz.co>
2026-03-12 16:24:33 -0700

    Add query-opportunities API response structure

---
+++ api/query-opportunities.ts
@@ -0,0 +1,24 @@
+{
+  ok: true,
+  opportunities: [
+    {
+      type: "ctr_opportunity",
+      url
+      query
+      impressions
+      clicks
+      position
+    },
+    {
+      type: "near_page_one",
+      query
+      position
+      url
+    },
+    {
+      type: "cannibalization",
+      query
+      pages: []
+    }
+  ]
+}
